4362cf3e510070641d233258f409074681553e13
user <user@172.21.57.176>
2018-03-13 16:53:10 +0100

    Verifica range 1...NMAX

--- src/it/polito/tdp/indonumero/IndoNumeroController.java
+++ src/it/polito/tdp/indonumero/IndoNumeroController.java
@@ -75,6 +75,11 @@ public class IndoNumeroController {
     	try {
     	int num = Integer.parseInt(numS);   
     	//numero è effetivamente un intero
+    	
+    	if(num<1 || num>NMAX) {
+    		txtLog.appendText("Valore fuori dall'intervallo consentito\n");
+    		return;
+    	}
     	if(num==this.segreto) {
     		//ha indovinato
     		txtLog.appendText("Hai vinto!\n");
